0d3a8e147162cd76206e2b40bd95d1b2c43cf2ac
VadimKolpashchikov <70956901+VadimKolpashchikov@users.noreply.github.com>
2023-08-23 20:17:31 +0300

    Отключает скользящий скролл для повышения производительности на слабых устройствах

--- web/new-site-vue/src/main.js
+++ web/new-site-vue/src/main.js
@@ -6,6 +6,6 @@ import "swiper/scss/effect-fade";
 import { createApp } from "vue";
 import App from "./vue/App.vue";
 import store from "./vue/store";
-import "./js/smoothScroll.js";
+// import "./js/smoothScroll.js";
 
 createApp(App).use(store).mount("#app");
